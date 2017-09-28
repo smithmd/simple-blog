a34d1f92cafb99c5bd6756e825b4faa21ccbe176
Michael Smith <mike@noiseparticles.com>
2017-09-28 07:46:56 -0400

    added error handling (404)

--- simple-blog.py
+++ simple-blog.py
@@ -23,24 +23,27 @@ def index():
 @app.route('/b/<content_type>/', defaults={'page_number': 0})
 @app.route('/b/<content_type>/<int:page_number>')
 def entry_list(content_type, page_number):
-    # TODO: fix crash if folder not found. Handle gracefully
-    markdown_files = os.listdir(os.path.join(app.static_folder, 'md_' + content_type))
-    file_list = ''
-    for md_file in markdown_files:
-        file_list += '<li>' + md_file + '</li>'
-    file_list = Markup(file_list)
-
-    return render_template('entry_list.html', **locals())
+    if os.path.exists(os.path.join(app.static_folder, 'md_' + content_type)):
+        markdown_files = os.listdir(os.path.join(app.static_folder, 'md_' + content_type))
+        file_list = ''
+        for md_file in markdown_files:
+            file_list += '<li>' + md_file + '</li>'
+        file_list = Markup(file_list)
+        return render_template('entry_list.html', **locals())
+    else:
+        return render_template('404.html', **locals())
 
 
 @app.route('/b/<content_type>/<entry_file>')
 def entry(content_type,entry_file):
-    file = os.path.join(app.static_folder, 'md_' + content_type + '/' + entry_file + '.md')
-    with open(file, 'r') as f:
-            md = f.read()
-    entry_content = Markup(Markdown.markdown(md))
-    return render_template('entry.html', **locals())
-
+    if os.path.exists(os.path.join(app.static_folder, 'md_' + content_type + '/' + entry_file + '.md')):
+        md_file = os.path.join(app.static_folder, 'md_' + content_type + '/' + entry_file + '.md')
+        with open(md_file, 'r') as f:
+                md = f.read()
+        entry_content = Markup(Markdown.markdown(md))
+        return render_template('entry.html', **locals())
+    else:
+        return render_template('404.html', **locals())
 
 if __name__ == '__main__':
     app.run()
